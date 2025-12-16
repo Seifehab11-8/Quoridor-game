738700ab1e1b431f8cc42d901bd5d634d04b0cce
Seifehab11-8 <semaziz2004@yahoo.com>
2025-12-16 13:11:39 +0200

    fixed some issues with releases and fonts of the titles

--- lib/views/widget_tree.dart
+++ lib/views/widget_tree.dart
@@ -13,7 +13,15 @@ class _WidgetTreeState extends State<WidgetTree> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Quoridor'),
+        title: Text(
+          'QUORIDOR',
+          style: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.w500, // The logo is relatively thin
+              letterSpacing: 3.0, // The logo has wide spacing
+              color: Colors.black, // or your dark brown color
+            ),
+        ),
       ),
       body: Board(),
     );
